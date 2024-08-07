8cbbf7810ec3dc71e9d723d9e3a18b7888e6eb1c
Jairo Antezana <jairo.antezana@gmail.com>
2024-08-06 23:48:48 -0500

    Feature Loading in List

--- app/components/commons/TableModel.tsx
+++ app/components/commons/TableModel.tsx
@@ -8,6 +8,7 @@ import { useRouter } from "next/navigation";
 import { useSelector } from "react-redux";
 import { RootState } from "@/app/theme/store";
 import PdfModal from "@/app/dashboard/attachments/ShowPdf";
+import { CheckIcon, ArrowPathIcon ,EnvelopeIcon } from "@heroicons/react/20/solid";
 
 // import { useRouter } from "next/router";
 
@@ -41,22 +42,28 @@ export default function TableModel({
   const segments = pathname.split("/");
   const lastSegment = segments[segments.length - 1];
   const isDarkMode = useSelector((state: RootState) => state.theme.isDarkMode);
+  const [pending, setPending] = useState(false);
+
 
   console.log(pathname);
   // const [checked, setChecked] = useState<{ [key: string]: boolean }>({});
   const router = useRouter();
 
   const handleCheckboxChange = (id: string, items: any) => {
+    setPending(true);
     router.push("/dashboard/delivery/" + id);
+    //setPending(false);
   };
 
   const handleRedirectCards = (id: string, items: any) => {
     // router.push("/dashboard/delivery/" + id);
+    setPending(true);
     router.push(`/dashboard/cards/${id}/${lastSegment}`);
   };
 
   const handleRedirectAttachment = (id: string, items: any) => {
     // router.push("/dashboard/delivery/" + id);
+    setPending(true);
     router.push(`/dashboard/attachments/${id}`);
   };
 
@@ -106,24 +113,55 @@ export default function TableModel({
                 {actionPdf && <PdfModal pdfUrl={base64?.downloadUrl} />}
 
                 {pathname === "/dashboard/projects" && (
+
                   <button
                     onClick={() => handleCheckboxChange(items.id, items)}
-                    className="bg-orange-500 text-white border-2 border-black px-4 py-2 rounded hover:bg-orange-600"
-                  />
+                    className="bg-orange-500 rounded-md border p-2 hover:bg-orange-600 disabled:opacity-50"
+                    disabled={pending}>
+                    {pending ? (
+                      <>
+                        <ArrowPathIcon className="animate-spin h-5 w-5 text-white" />
+                      </>
+                    ) : (
+                      <>
+                        <CheckIcon className="w-5 text-white" />
+                      </>
+                    )}
+                  </button>
                 )}
                 {!notUpdate && <Update id={items.id} route={"projects"} />}
                 {!notDelete && <Delete id={items.id} />}
                 {redirect && (
                   <button
                     onClick={() => handleRedirectCards(items.id, items)}
-                    className="bg-green-400-500 text-white border-2 border-black px-4 py-2 rounded hover:bg-green-600"
-                  />
+                    className="bg-green-500 rounded-md border p-2 hover:bg-green-600 disabled:opacity-50"
+                    disabled={pending}>
+                    {pending ? (
+                      <>
+                        <ArrowPathIcon className="animate-spin h-5 w-5 text-white" />
+                      </>
+                    ) : (
+                      <>
+                        <EnvelopeIcon className="w-5 text-white" />
+                      </>
+                    )}
+                  </button>
                 )}
                 {redirectAttachment && (
                   <button
                     onClick={() => handleRedirectAttachment(items.id, items)}
-                    className="bg-green-400-500 text-white border-2 border-black px-4 py-2 rounded hover:bg-green-600"
-                  />
+                    className="bg-green-500 rounded-md border p-2  hover:bg-green-600 disabled:opacity-50"
+                    disabled={pending}>
+                    {pending ? (
+                      <>
+                        <ArrowPathIcon className="animate-spin h-5 w-5 text-white" />
+                      </>
+                    ) : (
+                      <>
+                        <EnvelopeIcon className="w-5 text-white" />
+                      </>
+                    )}
+                  </button>
                 )}
               </div>
             </td>
